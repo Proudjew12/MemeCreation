199b501d11296692b3afee31f67996060bdae3fc
Proudjew12 <eran2214@gmail.com>
2025-08-14 12:27:37 +0300

    implement tags

--- JS/gallery-controller.js
+++ JS/gallery-controller.js
@@ -1,6 +1,20 @@
 'use strict'
 
-function onRenderGallery() {
+'use strict'
 
+function onRenderGallery() {
+    const tags = document.querySelectorAll('.tag');
+    tags.forEach(tag => {
+        const randomSize = Math.floor(Math.random() * 10) + 14; // 14px to 24px
+        tag.style.fontSize = `${randomSize}px`;
+    });
+
+    tags.forEach(tag => {
+        tag.addEventListener('click', () => {
+            tags.forEach(t => t.classList.remove('active'))
+            tag.classList.add('active')
+        });
+    });
 }
+
 
