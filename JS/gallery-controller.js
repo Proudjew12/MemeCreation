a189a88db0eea00ffdaf89fed01b11d4e4100454
Proudjew12 <eran2214@gmail.com>
2025-08-14 13:34:04 +0300

    implement gallery img render

--- JS/gallery-controller.js
+++ JS/gallery-controller.js
@@ -1,20 +1,43 @@
 'use strict'
 
-'use strict'
-
 function onRenderGallery() {
+
     const tags = document.querySelectorAll('.tag');
     tags.forEach(tag => {
         const randomSize = Math.floor(Math.random() * 10) + 14; // 14px to 24px
         tag.style.fontSize = `${randomSize}px`;
-    });
 
-    tags.forEach(tag => {
         tag.addEventListener('click', () => {
             tags.forEach(t => t.classList.remove('active'))
             tag.classList.add('active')
         });
     });
+
+    const elGallery = document.getElementById('gallery-container')
+
+    const folders = {
+        CartoonMan: 39,
+        Dog: 5,
+        Cat: 3,
+        Clown: 13,
+        Man: 7,
+        Women: 8,
+        Pepe: 24,
+        Spongbob: 17,
+    }
+
+    for (const folder in folders) {
+        const count = folders[folder]
+        for (let i = 1; i <= count; i++) {
+            const img = document.createElement('img')
+            img.src = `img/${folder}/${folder} ${i}.jpg`
+            img.alt = `${folder} ${i}`
+
+            const wrapper = document.createElement('div')
+            wrapper.classList.add('gallery-item')
+            wrapper.appendChild(img)
+
+            elGallery.appendChild(wrapper)
+        }
+    }
 }
-
-
